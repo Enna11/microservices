a7500349c8296f49238e435ae92777a84dbfd85d
Assia IDDIR <136933324+Enna11@users.noreply.github.com>
2023-09-05 20:42:45 +0200

    images.py

--- images_service/images.py
+++ images_service/images.py
@@ -168,4 +168,4 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5020)
+    app.run(host='0.0.0.0', debug=True, port=5008)
